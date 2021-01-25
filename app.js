c4180aee6fbd26d32bc5cc787d1b2b1a4dbab45f
Mark Edward Ratio <markedwardratio@gmail.com>
2021-01-25 11:53:03 +0800

    Set up default mongoose connection

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ require('dotenv').config();
 
 const express = require('express');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// Set up default mongoose connection
+const mongoDB = process.env.DEV_DB_STRING;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.use(logger('dev'));
 app.use(express.json());
