d17818985cb3b4d1141209fc74e118d444b573ee
Mark Edward Ratio <markedwardratio@gmail.com>
2021-01-26 20:37:39 +0800

    Add posts GET '/' route

--- routes/posts.js
+++ routes/posts.js
@@ -4,19 +4,7 @@ const passport = require('passport');
 
 const postsController = require('../controllers/postsController');
 
-router.get(
-	'/',
-	passport.authenticate('jwt', { session: false }),
-	(req, res, next) => {
-		// Dummy json response
-		res.json({
-			posts: [
-				{ title: 'Post 1', body: 'Hello World', published: false },
-				{ title: 'Post 2', body: 'Hi World', published: true },
-			],
-		});
-	}
-);
+router.get('/', postsController.index);
 
 router.post('/', postsController.create);
 
