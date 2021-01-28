a36dc791a8becd7cb79eb8cf48d8701346f53420
Mark Edward Ratio <markedwardratio@gmail.com>
2021-01-28 14:54:05 +0800

    Configure mongoose to remove deprecation warning

--- config/db.js
+++ config/db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
 const mongoDB = process.env.DEV_DB_STRING;
+// https://mongoosejs.com/docs/deprecations.html#findandmodify
+mongoose.set('useFindAndModify', false);
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
