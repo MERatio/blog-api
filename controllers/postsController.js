6fa60f871bdc783f219782675c651caec0f5f6c5
Mark Edward Ratio <markedwardratio@gmail.com>
2021-01-28 14:52:39 +0800

    Add posts#update

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -169,3 +169,60 @@ exports.edit = [
 		});
 	},
 ];
+
+exports.update = [
+	passport.authenticate('jwt', { session: false }),
+	// Validate and sanitise fields.
+	body('title')
+		.trim()
+		.isLength({ min: 1 })
+		.withMessage('Title is required')
+		.isLength({ max: 60 })
+		.withMessage('Title is too long (maximum is 60 characters)')
+		.escape(),
+	body('body')
+		.trim()
+		.isLength({ min: 1 })
+		.withMessage('Body is required')
+		.isLength({ max: 1000 })
+		.withMessage('Body is too long (maximum is 1000 characters)')
+		.escape(),
+	body('published')
+		.isBoolean()
+		.withMessage('Published field should be a boolean'),
+	// Process request after validation and sanitization.
+	(req, res, next) => {
+		// Extract the validation errors from a request.
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			// There are errors.
+			res.json({
+				post: req.body,
+				errors: errors.array(),
+			});
+		} else {
+			// Data is valid.
+			// Create a Post object with escaped and trimmed data and old id.
+			const post = new Post({
+				author: req.user._id,
+				title: req.body.title,
+				body: req.body.body,
+				published: req.body.published,
+				_id: req.params.id, // This is required, or a new ID will be assigned!
+			});
+			// Update the record.
+			Post.findByIdAndUpdate(req.params.id, post, {
+				new: true,
+				runValidators: true,
+			}).exec((err, updatedPost) => {
+				if (err) {
+					res.status(500).json({
+						errors: [{ msg: 'Something went wrong, please try again later' }],
+					});
+				} else {
+					res.json({ updatedPost });
+				}
+			});
+		}
+	},
+];
