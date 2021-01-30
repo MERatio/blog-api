6d6e139a77f0eb4109f20b554cdabce2963990d6
Mark Edward Ratio <markedwardratio@gmail.com>
2021-01-30 11:57:54 +0800

    Add comments#create

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -1,3 +1,5 @@
+const { body, validationResult } = require('express-validator');
+
 // Lib
 const beforeMiddlewares = require('../lib/beforeMiddlewares');
 
@@ -57,5 +59,81 @@ exports.new = [
 				}
 			}
 		);
+	},
+];
+
+exports.create = [
+	beforeMiddlewares.validMongooseObjectIdRouteParams({
+		postId: 'Post not found',
+	}),
+	// If admin is the one commenting user their username.
+	(req, res, next) => {
+		if (req.user) {
+			req.body.username = req.user.username;
+		}
+		next();
+	},
+	// Post should be published before commenting.
+	(req, res, next) => {
+		Post.findOne({ _id: req.params.postId, published: true }).exec(
+			(err, post) => {
+				if (err) {
+					next(err);
+				} else if (post === null) {
+					const err = new Error('Post not found');
+					err.status = 404;
+					next(err);
+				} else {
+					next();
+				}
+			}
+		);
+	},
+	// Validate and sanitise fields.
+	body('username')
+		.trim()
+		.isLength({ min: 1 })
+		.withMessage('Username is required')
+		.isLength({ max: 20 })
+		.withMessage('Username is too long (maximum is 20 characters)')
+		.escape(),
+	body('body')
+		.trim()
+		.isLength({ min: 1 })
+		.withMessage('Comment body is required')
+		.isLength({ max: 200 })
+		.withMessage('Comment body is too long (maximum is 200 characters)')
+		.escape(),
+	// Process request after validation and sanitization.
+	(req, res, next) => {
+		// Extract the validation errors from a request.
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			// There are errors.
+			res.json({
+				user: req.user ? req.user.forPublic : false,
+				comment: req.body,
+				errors: errors.array(),
+			});
+		} else {
+			// Data is valid.
+			// Create a Comment object with escaped and trimmed data.
+			const comment = new Comment({
+				post: req.params.postId,
+				username: req.body.username,
+				body: req.body.body,
+			});
+			comment.save((err, comment) => {
+				if (err) {
+					next(err);
+				} else {
+					// Successful
+					res.json({
+						user: req.user ? req.user.forPublic : false,
+						comment,
+					});
+				}
+			});
+		}
 	},
 ];
