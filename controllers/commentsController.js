5b30fb28f892da48bc6780cf105455d91c78d45d
Mark Edward Ratio <markedwardratio@gmail.com>
2021-02-01 14:57:44 +0800

    Add comments#update

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -134,3 +134,53 @@ exports.edit = [
 		});
 	},
 ];
+
+exports.update = [
+	beforeMiddlewares.jwtAuthenticated,
+	beforeMiddlewares.validMongooseObjectIdRouteParams,
+	beforeMiddlewares.authorizedToUpdateComment,
+	// Validate and sanitise fields.
+	body('body')
+		.trim()
+		.isLength({ min: 1 })
+		.withMessage('Comment body is required')
+		.isLength({ max: 200 })
+		.withMessage('Comment body is too long (maximum is 200 characters)')
+		.escape(),
+	// Process request after validation and sanitization.
+	(req, res, next) => {
+		// Extract the validation errors from a request.
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			// There are errors.
+			res.json({
+				user: req.user ? req.user.forPublic : false,
+				comment: req.body,
+				errors: errors.array(),
+			});
+		} else {
+			// Data is valid.
+			// Create a Comment object with escaped and trimmed data and old id.
+			const comment = new Comment({
+				author: req.user._id,
+				post: req.params.postId,
+				body: req.body.body,
+				_id: req.params.commentId, // This is required, or a new ID will be assigned!
+			});
+			// Update the record.
+			Comment.findByIdAndUpdate(req.params.commentId, comment, {
+				new: true,
+				runValidators: true,
+			}).exec((err, updatedComment) => {
+				if (err) {
+					next(err);
+				} else {
+					res.json({
+						user: req.user.forPublic,
+						comment: updatedComment,
+					});
+				}
+			});
+		}
+	},
+];
