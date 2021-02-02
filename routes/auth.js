e58c8b02401769408ec248ac5766121048fbbd21
Mark Edward Ratio <markedwardratio@gmail.com>
2021-02-02 11:49:32 +0800

    Add GET /auth/sign-in route

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const authController = require('../controllers/authController');
 
+router.get('/sign-in', authController.new);
+
 router.post('/', authController.create);
 
 module.exports = router;
