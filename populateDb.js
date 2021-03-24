779477a26524e6164381b0a2d532dbfa8cbf6ab4
Mark Edward Ratio <markedwardratio@gmail.com>
2021-03-24 12:56:13 +0800

    Update and add data to populateDb.js

--- populateDb.js
+++ populateDb.js
@@ -115,7 +115,7 @@ const createPosts = (cb) => {
       (callback) => {
         postCreate(
           users[0],
-          'Unpublished post',
+          'Unpublished post 1',
           `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ultricies commodo dignissim. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam interdum sem quam, a porttitor massa volutpat a. Etiam mollis accumsan ultricies. Integer mattis volutpat efficitur. Praesent justo eros, rhoncus eu est sit amet, ultrices lacinia dui. In interdum ex a dolor porta viverra. Sed justo ligula, aliquet ut nisi et, rutrum aliquam nulla. Donec tristique in mi a accumsan. Praesent commodo dui augue. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum libero nibh, ullamcorper facilisis elementum in, aliquet pellentesque mi. Aliquam facilisis maximus dolor nec bibendum. Vivamus id auctor sapien.`,
           false,
           callback
@@ -124,7 +124,25 @@ const createPosts = (cb) => {
       (callback) => {
         postCreate(
           users[0],
-          'Published post',
+          'Unpublished post 2',
+          `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ultricies commodo dignissim. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam interdum sem quam, a porttitor massa volutpat a. Etiam mollis accumsan ultricies. Integer mattis volutpat efficitur. Praesent justo eros, rhoncus eu est sit amet, ultrices lacinia dui. In interdum ex a dolor porta viverra. Sed justo ligula, aliquet ut nisi et, rutrum aliquam nulla. Donec tristique in mi a accumsan. Praesent commodo dui augue. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum libero nibh, ullamcorper facilisis elementum in, aliquet pellentesque mi. Aliquam facilisis maximus dolor nec bibendum. Vivamus id auctor sapien.`,
+          false,
+          callback
+        );
+      },
+      (callback) => {
+        postCreate(
+          users[0],
+          'Published post 1',
+          `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in ultricies enim. Nam ultricies maximus massa, id tempus nisi sagittis et. Sed nec tempus eros. Nam nec odio sagittis dui malesuada tempus. Mauris tincidunt nunc eu quam euismod rutrum. Quisque nibh justo, dapibus vel aliquam porta, malesuada nec neque. Maecenas faucibus lacus ipsum, eu tincidunt odio pulvinar a. Aliquam erat volutpat. Quisque ipsum eros, imperdiet vel nisi et, facilisis aliquet quam. Aliquam sit amet nibh placerat, euismod dolor vel, pharetra arcu. Phasellus pulvinar vehicula dui, quis tincidunt risus euismod a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget accumsan augue, eu consequat ante. Nunc non volutpat sem. Mauris eget enim et purus placerat viverra sed quis nisl. Curabitur in mi eu dolor consequat fringilla vel eget metus.`,
+          true,
+          callback
+        );
+      },
+      (callback) => {
+        postCreate(
+          users[0],
+          'Published post 2',
           `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in ultricies enim. Nam ultricies maximus massa, id tempus nisi sagittis et. Sed nec tempus eros. Nam nec odio sagittis dui malesuada tempus. Mauris tincidunt nunc eu quam euismod rutrum. Quisque nibh justo, dapibus vel aliquam porta, malesuada nec neque. Maecenas faucibus lacus ipsum, eu tincidunt odio pulvinar a. Aliquam erat volutpat. Quisque ipsum eros, imperdiet vel nisi et, facilisis aliquet quam. Aliquam sit amet nibh placerat, euismod dolor vel, pharetra arcu. Phasellus pulvinar vehicula dui, quis tincidunt risus euismod a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget accumsan augue, eu consequat ante. Nunc non volutpat sem. Mauris eget enim et purus placerat viverra sed quis nisl. Curabitur in mi eu dolor consequat fringilla vel eget metus.`,
           true,
           callback
@@ -143,7 +161,7 @@ const createComments = (cb) => {
         commentCreate(
           users[0],
           posts[0],
-          'Comment on unpublished post (the post is published before when someone commented on it then became unpublished)',
+          'Comment on unpublished post (the post is published before when someone commented on it then became unpublished) 1',
           callback
         );
       },
@@ -151,23 +169,23 @@ const createComments = (cb) => {
         commentCreate(
           users[1],
           posts[0],
-          'Comment on unpublished post (the post is published before when someone commented on it then became unpublished)',
+          'Comment on unpublished post (the post is published before when someone commented on it then became unpublished) 2',
           callback
         );
       },
       (callback) => {
         commentCreate(
           users[0],
-          posts[1],
-          'Comment on published post',
+          posts[2],
+          'Comment on published post 1',
           callback
         );
       },
       (callback) => {
         commentCreate(
           users[1],
-          posts[1],
-          'Comment on published post',
+          posts[2],
+          'Comment on published post 2',
           callback
         );
       },
